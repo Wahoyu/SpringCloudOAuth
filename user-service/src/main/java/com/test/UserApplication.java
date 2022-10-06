96229b7f2b03eda5d938ee74f4086094caa75f8f
王泓宇 <645702818@qq.com>
2022-10-06 16:02:51 +0800

    添加了AuthService服务，搭建验证服务器

---
+++ user-service/src/main/java/com/test/UserApplication.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.awt.print.Book;
+
+@SpringBootApplication
+public class UserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class,args);
+    }
+}
